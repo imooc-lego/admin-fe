9f834da80eb92c2753b00a0e35327caee19830da
王福朋 <wangfupeng1988@163.com>
2020-10-20 17:39:47 +0800

    chore: 发布上线

--- src/config/host.ts
+++ src/config/host.ts
@@ -9,14 +9,14 @@ import { isDevNoMock, isPrd, isPrdDev } from '../utils/env'
 let APIHost = '' // 默认为本地运行 mock
 if (isDevNoMock) APIHost = 'http://localhost:3003' // 本地，不用 mock
 if (isPrdDev) APIHost = 'http://182.92.168.192:8084' // 测试机
-if (isPrd) APIHost = 'yyy' // TODO 线上环境
+if (isPrd) APIHost = '' // 线上环境，用当前域名即可
 
 // h5 host
 let H5Host = 'http://182.92.168.192:8082'
-if (isPrd) H5Host = 'https://www.imooc-lego.com'
+if (isPrd) H5Host = 'https://h5.imooc-lego.com'
 
 // 统计接口的 host
 let StatHost = 'http://182.92.168.192:8080'
-if (isPrd) StatHost = 'xxx' // TODO 线上环境
+if (isPrd) StatHost = 'https://statistic-res.imooc-lego.com' // 线上环境
 
 export { APIHost, H5Host, StatHost }
