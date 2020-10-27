b3aade434060ea1772a55a56883404972c7c46f5
王福朋 <wangfupeng1988@163.com>
2020-10-27 16:43:11 +0800

    feat: 404 页面 UI

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Result, Button } from 'antd'
+import { history } from 'umi'
 
 export default () => {
     return (
-        <div>
-            <h1>404 Not Found</h1>
-        </div>
+        <Result
+            status="404"
+            title="404"
+            subTitle="您访问的页面不存在"
+            extra={
+                <Button
+                    type="primary"
+                    onClick={() => {
+                        history.push('/')
+                    }}
+                >
+                    返回首页
+                </Button>
+            }
+        />
     )
 }
